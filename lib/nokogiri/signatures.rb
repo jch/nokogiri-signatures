a1b40ba171dacad84844e3424869e1cef2a0cf66
Jerry Cheung <jch@whatcodecraves.com>
2015-02-06 14:08:54 -0800

    bundle gem nokogiri-signatures

---
+++ lib/nokogiri/signatures.rb
@@ -0,0 +1,7 @@
+require "nokogiri/signatures/version"
+
+module Nokogiri
+  module Signatures
+    # Your code goes here...
+  end
+end
---
+++ lib/nokogiri/signatures/version.rb
@@ -0,0 +1,5 @@
+module Nokogiri
+  module Signatures
+    VERSION = "0.0.1"
+  end
+end
